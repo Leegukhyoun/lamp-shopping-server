1e2e5e90f813af7c51b05d086e1a20fc6fcfe11d
LeegukHyoun <dbkim512@naver.com>
2022-07-05 09:15:37 +0900

    서버 배포 준비 완료

--- index.js
+++ index.js
@@ -1,43 +1,141 @@
-//node는 CommonJS문법을 사용
-//import require()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+//헤로쿠에서 포트 지정하는게 있으면 그 번호 사용
+//없으면 8080포트 사용
+const port = precess.env.PORT || 8080;
+const models = require('./models');
 
-const http = require('http');
+//업로드 이미지를 관리하는 스토리지 서버를 연결 -> 멀터를 사용하겠다.
+const multer = require("multer");
 
-// 내 컴퓨터의 주소 127.0.0.1
-const hostname = "127.0.0.1";
-const port = 8080;
-
-//서버만들기 createServer(function(req, res))
-                                //요청, 응답
-
-const server = http.createServer(function(req, res){
-    //요청정보 req
-    //응답해줄게 res
-    const path = req.url;
-    const method = req.method;
-    if (path === "/products"){
-        if(method === "GET"){
-            //응답을 보낼 때 타입을 제이슨 객체를 헤더에 보낼거야
-            res.writeHead(200, {'Content-Type':'application/json'})
-            //js 배열을 json형태로 벼녕해서 products에 담기 
-            const products = JSON.stringify([
-                {
-                    name : "거실조명",
-                    price: 50000,
-                },
-                {
-                    name: "어린이조명",
-                    price: 5050050
-                }
-            ])
-            res.end(products);
+//이미지 파일 요청이 오면 어디에 저장할건지 지정
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function(req, file, cb){
+            //어디에 저장할건지? upload/
+            cb(null, 'upload/')
+        },
+        filename : function(req, file, cb){
+            //어떤 이름으로 지정할건지?
+            //file객체의 오리지널 이름으로 저장하겠다
+            cb(null, file.originalname)
         }
-    }
-    console.log(path);
-    console.log(method);
-    res.end("Hello Client");
+    })
+});
+
+//json 방식의 데이터를 처리할 수 있게 설정
+app.use(express.json());
+
+//브라우저 cors 이슈를 막기 위해 사용 (모든 브라우저의 요청을 일정하게 받겠다)
+app.use(cors());
+
+//업로드 폴더에 있는 파일에 접근할 수 있도록 설정
+app.use("/upload", express.static("upload"));
+
+
+//요청처리
+//app.메서드(url, 함수)
+//이미지 파일이 post로 요청이 왔을 때 upload라는 폴더에 이미지를 저장하기
+//이미지가 하나일 때 single
+app.post('/image', upload.single('image'), (req, res)=>{
+    const file = req.file;
+    console.log(file);
+    res.send({
+        imageUrl : "http://localhost:3000/"+file.destination+file.filename
+    })
+});
+
+
+app.get('/products', async (req, res)=>{
+    //데이터베이스 조회하기
+    models.Product.findAll()
+    .then(result=>{
+        console.log("제품 전체조회", result);
+        res.send(result);
+        // res.send({
+        //     product : result
+        // });
+    })
+    .catch(e=>{
+        console.error(e);
+        res.send("파일 조회에 문제가 있습니다.")
+    })
+});
+
+//method get요청이 오고 url은 /product/2 로 요청이 왔을 때
+app.get('/product/:id', async (req, res)=>{
+    const params = req.params;
+    // const { id } = params;
+    //하나만 조회할 때는 findOne -> select문
+    models.Product.findOne({
+        //조건절
+        where : {
+            id : params.id
+        }
+    })
+    .then(result=>{
+        res.send(result);
+    })
+    .catch(e=>{
+        console.log(e);
+        res.send('상품 조회에 문제가 생겼습니다.')
+    })
+});
+app.post('/green', async (req, res)=>{
+    console.log(req);
+    res.send('게시글이 등록되었씁니다.')
 })
 
-//listen은 대기 호스트네임과 포트번호로 요청을 기다린다
-server.listen(port, hostname);
-console.log('조명쇼핑몰 서버가 돌아가고 있습니다.');
+app.post("/products", (req, res)=>{
+    //http body에 있는 데이터
+    const body = req.body;
+    //body 객체에 있는 값을 구조분해 할당 
+    const { name, price, seller, imageUrl, description} = body;
+    if(!name || !price || !seller){
+        res.send("모든 항목을 입력해주세요.");
+    }
+    //Product테이블에 레코드를 삽입
+    models.Product.create({
+        name,
+        price,
+        seller,
+        imageUrl,
+        description,
+    }).then(result=>{
+        console.log('상품 생성 결과 : ', result);
+        res.send({
+            result
+        });
+    })
+    .catch(e=>{
+        console.log(error);
+        res.send("상품 업로드에 문제가 생겼습니다.");
+    })
+})
+
+//delete삭제하기
+app.delete('/product/:id', async (req, res) => {
+    const params = req.params;
+    models.Product.destroy({ where: { id: params.id }})
+    .then(res.send("상품이 삭제되었습니다"));
+})
+
+//실행
+app.listen(port, ()=>{
+    console.log('쇼핑몰 서버가 동작중입니다.');
+
+    //sequelize와 데이터베이스 연결 작업
+    //데이터베이스 동기화
+    models.sequelize
+    .sync()
+    .then(()=>{
+        console.log('DB연결 성공');
+    })
+    .catch(e=>{
+        console.error(e);
+        console.log('DB연결 에러');
+        //서버 실행이 안되면 프로세서 종료
+        process.exit();
+    })
+})
